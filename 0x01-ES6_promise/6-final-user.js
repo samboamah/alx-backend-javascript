eced8a8a7e0a04f52b6421a30bc6eb2cef0cebdf
Samuel Boamah <skboamah16@gmail.com>
2023-07-06 18:35:29 +0000

    add

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,25 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName).catch((error) => {
-      return error;
-    }),
-  ]).then((values) => {
-    let status;
-
-    const responses = [];
-    values.forEach((value) => {
-      if (value instanceof Error) status = 'failed';
-      else status = 'fulfilled';
-      responses.push({
-        status,
-        value,
-      });
-    });
-
-    return responses;
-  });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => (
+      res.map((o) => ({
+        status: o.status,
+        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      }))
+    ));
 }
