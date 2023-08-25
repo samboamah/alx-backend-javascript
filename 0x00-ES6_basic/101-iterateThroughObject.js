230a91a686519227aa17e23a0d9fadede1f4ec2d
Samuel Boamah <skboamah16@gmail.com>
2023-08-25 13:43:43 +0000

    update

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,14 +1,9 @@
-export default function createReportObject(employeesList) {
-  const allEmployees = {};
+export default function iterateThroughObject(reportWithIterator) {
+  const employees = [];
 
-  for (const [departmentName, employees] of Object.entries(employeesList)) {
-    allEmployees[departmentName] = [...employees];
+  for (const employee of reportWithIterator) {
+    employees.push(employee);
   }
 
-  return {
-    allEmployees,
-    getNumberOfDepartments() {
-      return Object.keys(employeesList).length;
-    },
-  };
+  return employees.join(' | ');
 }
