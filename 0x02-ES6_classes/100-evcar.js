c4e4746ad62a098146b767e6e1c4341f11fe0a3a
Samuel Boamah <skboamah16@gmail.com>
2023-08-25 13:54:13 +0000

    update

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,14 +1,13 @@
 import Car from './10-car';
 
-class EVCar extends Car {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
   }
 
-  static get [Symbol.species]() {
-    return Car;
+  // eslint-disable-next-line class-methods-use-this
+  cloneCar() {
+    return new Car();
   }
 }
-
-export default EVCar;
